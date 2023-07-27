fd93c8cc20aef7a4d7795a460cdeb96a081c80ed
farhaz <farhaz@zonemultiverse.com>
2023-07-27 18:20:53 +0400

    add eureka netflix

--- player-service/src/main/java/com/fantasyleague/playerservice/controller/PlayerController.java
+++ player-service/src/main/java/com/fantasyleague/playerservice/controller/PlayerController.java
@@ -3,6 +3,9 @@ package com.fantasyleague.playerservice.controller;
 import com.fantasyleague.playerservice.dto.PlayerRequest;
 import com.fantasyleague.playerservice.model.Player;
 import com.fantasyleague.playerservice.service.PlayerService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.security.PrivateKey;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/players")
@@ -19,6 +23,9 @@ public class PlayerController {
     private final PlayerService playerService;
 
     @PostMapping
+    @CircuitBreaker(name="player", fallbackMethod = "fallbackMethod")
+    @TimeLimiter(name="player")
+    @Retry(name = "player")
     public ResponseEntity<Player> createPlayer(@RequestBody PlayerRequest playerRequest) {
         Player playerResponse = playerService.createPlayer(playerRequest);
         return new ResponseEntity<>(playerResponse, HttpStatus.CREATED);
@@ -32,5 +39,9 @@ public class PlayerController {
         } else {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
+    }
+
+    public CompletableFuture<String> fallbackMethod(RuntimeException runtimeException){
+        return CompletableFuture.supplyAsync(()-> "Oops! Something went wrong, please try again later!");
     }
 }
